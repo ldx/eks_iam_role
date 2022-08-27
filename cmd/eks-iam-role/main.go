4764eca832eead08a77de68d58c22e22f96a4bee
Vilmos Nebehaj <v.nebehaj@gmail.com>
2022-08-27 09:06:07 -0700

    Update flags

--- cmd/eks-iam-role/main.go
+++ cmd/eks-iam-role/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/jessevdk/go-flags"
 	"github.com/ldx/eks_iam_role/pkg/awswrapper"
@@ -10,19 +11,19 @@ import (
 
 var opts struct {
 	RoleName       string `long:"role-name" description:"Name of role to ensure" env:"ROLE_NAME" required:"true"`
-	PolicyName     string `long:"policy-name" description:"Name of policy that will be ensured, by default it will be same as the role name" env:"POLICY_NAME"`
+	PolicyName     string `long:"policy-name" description:"Name of policy that will be ensured, by default it will be same as role name" env:"POLICY_NAME"`
 	PolicyFilePath string `long:"policy-file-path" description:"Path of policy JSON file" value-name:"FILE" env:"POLICY_FILE_PATH" required:"true"`
 	AWSRegion      string `long:"aws-region" description:"AWS region" env:"AWS_REGION" required:"true"`
 	AWSEndpoint    string `long:"aws-endpoint" description:"AWS endpoint URL" env:"AWS_ENDPOINT" default:""`
-	ClusterName    string `long:"cluster-name" description:"Get OIDC issuer from cluster for creating role, either cluster-name or oidc-issue needs to be set" env:"CLUSTER_NAME"`
-	OIDCIssuer     string `long:"oidc-issuer" description:"Create role trust policy based on OIDC issuer for creating role, either cluster-name or oidc-issue needs to be set" env:"OIDC_ISSUER"`
+	ClusterName    string `long:"cluster-name" description:"Get OIDC issuer from cluster for creating role, either cluster-name or oidc-issuer needs to be set" env:"CLUSTER_NAME"`
+	OIDCIssuer     string `long:"oidc-issuer" description:"Create role trust policy based on OIDC issuer for creating role, either cluster-name or oidc-issuer needs to be set" env:"OIDC_ISSUER"`
 	Namespace      string `long:"namespace" description:"Namespace of the service account for which an IAM role association will be created" env:"NAMESPACE" required:"true"`
 	ServiceAccount string `long:"service-account" description:"Name of service account for which an IAM role association will be created" env:"SERVICE_ACCOUNT" required:"true"`
 }
 
 func main() {
 	if _, err := flags.Parse(&opts); err != nil {
-		log.Fatalf("Parsing flags: %v", err)
+		os.Exit(1)
 	}
 	if opts.OIDCIssuer == "" && opts.ClusterName == "" {
 		log.Fatal("Either --oidc-issuer or --cluster-name need to be set")
